43f23218ea69d44c1e7eca3a367838a48d60a170
Tom Doughty <thomashdoughty@gmail.com>
2020-12-14 11:28:39 +0000

    Use sass and optimise pngs

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
+const sass = require('gulp-sass');
+const cleanCss = require('gulp-clean-css');
 const nunjucksRender = require('gulp-nunjucks-render');
 
 function cleanDist() {
-  return gulp.src('dist', { allowEmpty: true })
+  return gulp.src('./dist', { allowEmpty: true })
     .pipe(clean());
 }
+
+function compileCSS() { 
+  return gulp.src('./src/main.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(cleanCss())
+    .pipe(gulp.dest('./src'));
+};
 
 function buildPages() {
   return gulp.src([
@@ -16,10 +26,17 @@ function buildPages() {
       'src/'
     ]
   }))
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('./dist'));
 };
+
+function cleanTemp() {
+  return gulp.src('./src/main.css', { allowEmpty: true })
+    .pipe(clean());
+}
 
 gulp.task('default', gulp.series([
   cleanDist,
+  compileCSS,
   buildPages,
+  cleanTemp,
 ]));
